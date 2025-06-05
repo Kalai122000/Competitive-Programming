25c1f33513a578ec093d31df9b395c96dcfb7ad8
Kalaiyarasan M <149670028+Kalai122000@users.noreply.github.com>
2025-06-05 10:15:13 +0530

    Update inner_reduce_pattern.cpp

--- General Questions/inner_reduce_pattern.cpp
+++ General Questions/inner_reduce_pattern.cpp
@@ -60,7 +60,47 @@ int main()
 	int n; 
     cin>>n;
 	innerPattern(n); 
-	
+
 return 0; 
 } 
 
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#include <stdio.h>
+
+#define MAX 100
+
+void print(int a[][MAX], int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            printf("%d", a[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void innerPattern(int n) {
+    int size = 2 * n - 1;
+    int front = 0;
+    int back = size - 1;
+    int a[MAX][MAX];
+    
+    while (n != 0) {
+        for (int i = front; i <= back; i++) {
+            for (int j = front; j <= back; j++) {
+                if (i == front || i == back || j == front || j == back)
+                    a[i][j] = n;
+            }
+        }
+        ++front;
+        --back;
+        --n;
+    }
+    print(a, size);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    innerPattern(n);
+    return 0;
+}
